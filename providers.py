17a69b028cb57c7e497e337b24e8cdf6390e8a6a
Artem Gromov <artyomgromov@ya.ru>
2016-05-28 01:21:28 +0300

    add youtube video and additional photo parsing

--- providers.py
+++ providers.py
@@ -23,6 +23,7 @@ class DancesportRu(Provider):
 
         self.full_name = None
         self.images = set()
+        self.videos = set()
         self.number = None
         self.description = None
         self.birth = None
@@ -36,6 +37,7 @@ class DancesportRu(Provider):
         self.date = None
 
         self.changed = set()
+        self.commited = True
 
     def update_attribute(self, name, new):
         self.log.debug('trying to update "{}" with: "{}" '.format(name, new))
@@ -54,120 +56,143 @@ class DancesportRu(Provider):
                     self.changed.add(name)
 
     def update(self):
-        self.changed = set()
-        
-        page = requests.get(self.url)
-        self.log.debug('responce code "{}"'.format(page.status_code))
-        if page.status_code == 200:
-            soup = BeautifulSoup(page.text, "html.parser")
+        if self.commited:
+            self.changed = set()
+            self.commited = False
 
-            # Get tag <h1> with name
-            full_name = soup.find('h1')
-            if full_name:
-                full_name = full_name.text.strip()
-                self.update_attribute('full_name', full_name)
+            page = requests.get(self.url)
+            self.log.debug('responce code "{}"'.format(page.status_code))
+            if page.status_code == 200:
+                soup = BeautifulSoup(page.text, "html.parser")
 
-            # Get target div <div class="one-adv cf">
-            target_div = soup.find('div', {'class': 'one-adv cf'})
-            if target_div:
+                # Get tag <h1> with name
+                full_name = soup.find('h1')
+                if full_name:
+                    full_name = full_name.text.strip()
+                    self.update_attribute('full_name', full_name)
 
-                # Get main image
-                main_image = target_div.find('div', {'class': 'img'}).find('img').get('src')
-                if main_image != '/images/empty-img.png':
-                    main_image = self.base_url + main_image
-                    self.update_attribute('images', main_image)
+                # Get target div <div class="one-adv cf">
+                target_div = soup.find('div', {'class': 'one-adv cf'})
+                if target_div:
 
-                # Get id
-                number = int(target_div.find('div', {'class': 'descr'}).find('div', {'class': 'numb'}).text[1:])
-                self.update_attribute('number', number)
+                    # Get main image
+                    image_div = target_div.find('div', {'class': 'img'})
+                    main_image = image_div.find('img').get('src')
+                    if main_image != '/images/empty-img.png':
+                        image = self.base_url + image_div.find('img').get('data-colorbox')
+                        self.update_attribute('images', image)
 
-                # Get description
-                try:
-                    description = target_div.find('div', {'class': 'descript'}).find('p', {'class': ''}).text
-                    description = description.replace('\n', ' ').replace('\r', ' ').replace('  ', ' ')
-                    self.update_attribute('description', description)
+                        images = image_div.find_all('div',{'class': 'one-img cboxElement'})
+                        if images:
+                            for tag in images:
+                                image = self.base_url + tag.get('data-colorbox')
+                                self.update_attribute('images', image)                        
 
-                except AttributeError:
-                    pass
+                    # Get videos
+                    videos = target_div.find_all('iframe',{'class':'youtubeframe'})
+                    for tag in videos:
+                        video = 'https://www.youtube.com/watch?v=' + tag.get('src').split('/')[-1]
+                        self.update_attribute('videos', video)
 
-                # Get notes data
-                notes = target_div.find('div', {'class': 'descr'}).find_all('p', {'class': 'note'})
-                notes = [i.text.strip().replace(': ', ':') for i in notes]
+                    # Get id
+                    number = int(target_div.find('div', {'class': 'descr'}).find('div', {'class': 'numb'}).text[1:])
+                    self.update_attribute('number', number)
 
-                # Parse note data
-                for i in notes:
-                    if i.startswith('Год рождения'):
-                        birth = int(i.split(':')[1])
-                        self.update_attribute('birth', birth)
-                        continue
+                    # Get description
+                    try:
+                        description = target_div.find('div', {'class': 'descript'}).find('p', {'class': ''}).text
+                        description = description.replace('\n', ' ').replace('\r', ' ').replace('  ', ' ')
+                        self.update_attribute('description', description)
 
-                    if i.startswith('Город'):
-                        city = i.split(':')[1].split(', ')[0]
-                        self.update_attribute('city', city)
+                    except AttributeError:
+                        pass
 
-                        country = i.split(':')[1].split(', ')[1]
-                        self.update_attribute('country', country)
+                    # Get notes data
+                    notes = target_div.find('div', {'class': 'descr'}).find_all('p', {'class': 'note'})
+                    notes = [i.text.strip().replace(': ', ':') for i in notes]
 
-                        continue
+                    # Parse note data
+                    for i in notes:
+                        if i.startswith('Год рождения'):
+                            birth = int(i.split(':')[1])
+                            self.update_attribute('birth', birth)
+                            continue
 
-                    if i.startswith('Рост'):
-                        height = int(i.split(':')[1].split(' ')[0])
-                        self.update_attribute('height', height)
+                        if i.startswith('Город'):
+                            city = i.split(':')[1].split(', ')[0]
+                            self.update_attribute('city', city)
 
-                        continue
+                            country = i.split(':')[1].split(', ')[1]
+                            self.update_attribute('country', country)
 
-                    if i.startswith('Класс'):
-                        class_string = i.split(':')[1].strip()
-                        if class_string.startswith('St'):
-                            class_st = class_string[4]
-                            self.update_attribute('class_st', class_st)
+                            continue
 
-                            if len(i) > 6:
-                                class_la = class_string[12]
+                        if i.startswith('Рост'):
+                            height = int(i.split(':')[1].split(' ')[0])
+                            self.update_attribute('height', height)
+
+                            continue
+
+                        if i.startswith('Класс'):
+                            class_string = i.split(':')[1].strip()
+                            if class_string.startswith('St'):
+                                class_st = class_string[4]
+                                self.update_attribute('class_st', class_st)
+
+                                if len(i) > 6:
+                                    class_la = class_string[12]
+                                    self.update_attribute('class_la', class_la)
+
+                                continue
+
+                            if class_string.startswith('La'):
+                                class_la = class_string[4]
                                 self.update_attribute('class_la', class_la)
 
+                                continue
+
+                        if i.startswith('Клуб'):
+                            club = i.split(':')[1]
+                            self.update_attribute('club', club)
+
                             continue
 
-                        if class_string.startswith('La'):
-                            class_la = class_string[4]
-                            self.update_attribute('class_la', class_la)
-
+                        else:
                             continue
 
-                    if i.startswith('Клуб'):
-                        club = i.split(':')[1]
-                        self.update_attribute('club', club)
+            else:
+                self.log.error('cannot load page "{}"'.format(self.url))
 
-                        continue
-
-                    else:
-                        continue
-
+            return self.changed
         else:
-            self.log.error('cannot load page "{}"'.format(self.url))
+            self.log.error('uncommited data exists for attributes: {}.'.format(', '.join(self.changed)))
 
-        return self.changed
+    def merge(self):
+        new_partner = Partner(1)
 
+        return new_data
 
 if __name__ == '__main__':
     from time import sleep
+    from copy import deepcopy
 
     logging.basicConfig(level=logging.DEBUG)
 
-    def print_diff(init, upd):
-        print('-'*150)
-        print('attribute name'.ljust(50) + 'init value'.ljust(50) + 'updated value'.ljust(50))
-        print('-'*150)
-        for key in init.keys():
-            print(key.ljust(50) + str(init[key]).ljust(50) + str(upd[key]).ljust(50))
+    def print_attr(instance):
+        print('-'*100)
+        print('{:50.50}{}'.format('attribute name','value'))
+        print('-'*100)
+        for k, v in instance.__dict__.items():
+            print('{:50.50}{}'.format(k,v))
 
-    url = 'http://dancesport.ru/partners/partners_8897.html'
-    provider = DancesportRu(url)
+    url1 = 'http://dancesport.ru/partners/partners_9324.html'
+    url2 = 'http://dancesport.ru/partners/partners_9102.html'
+    url3 = 'http://dancesport.ru/partners/partners_9472.html'
+    provider = DancesportRu(url3)
 
-    init_attrs = provider.__dict__.copy()
+    print_attr(provider)
+    sleep(1)
     provider.update()
-    upd_attrs = provider.__dict__.copy()
-
-    sleep(2)
-    print_diff(init_attrs, upd_attrs)
+    sleep(1)
+    print_attr(provider)
     
