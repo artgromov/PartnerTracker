8beba321fea71ce755301aed02922f5560839f2d
Artem Gromov <artyomgromov@ya.ru>
2016-06-17 18:44:48 +0300

    create status class and objects

--- lib/partner_tracker/partner.py
+++ lib/partner_tracker/partner.py
@@ -5,18 +5,43 @@ import re
 logger = logging.getLogger(__name__)
 
 
-class Partner:
-    NEW = 'Created'
-    UPD = 'First time updated'
-    ACK = 'First contact attempt'
-    WAIT = 'Awaiting additional info'
-    PTEST = 'Primary testing scheduled'
-    ATEST = 'Advanced testing'
-    IGN = 'Ignored'
-    NOAN = 'Still no answer'
-    NA = 'Source not found'
-    DEL = 'Should be deleted'
+class State:
+    states = [('S_NEW', 'Created'),
+              ('S_UPD', 'First time updated'),
+              ('S_ACK', 'First contact attempt'),
+              ('S_WAIT', 'Awaiting additional info'),
+              ('S_PTEST', 'Primary testing scheduled'),
+              ('S_ATEST', 'Advanced testing'),
+              ('S_IGN', 'Ignored'),
+              ('S_NOAN', 'No answer')
+              ]
 
+    state_set = set([i[0] for i in states])
+    state_dict = {k: v for k, v in states}
+
+    def __init__(self, state):
+        if state in self.state_set:
+            self.state = state
+        else:
+            raise KeyError
+
+    def __repr__(self):
+        return 'State(%s)' % self.state
+
+    def __str__(self):
+        return self.state_dict[self.state]
+
+S_NEW = State('S_NEW')
+S_UPD = State('S_UPD')
+S_ACK = State('S_ACK')
+S_WAIT = State('S_WAIT')
+S_PTEST = State('S_PTEST')
+S_ATEST = State('S_ATEST')
+S_IGN = State('S_IGN')
+S_NOAN = State('S_NOAN')
+
+
+class Partner:
     _schedule_pattern = re.compile('^(?P<rate>[0-9])p(?P<num>[0-9]?)(?P<interval>[wmy])')
     _schedule_intervals = {'w': 'week',
                            'm': 'month',
@@ -25,7 +50,7 @@ class Partner:
 
     def __init__(self):
         self.id = uuid.uuid4()
-        self.state = Partner.NEW
+        self.state = S_NEW
         self.providers = []
         self.conflicts = dict()
 
@@ -124,9 +149,11 @@ class Partner:
                 for name, new_value in new_data.items():
                     local_counter += self.update_attribute(name, new_value)
 
-            provider.reset_changes()
+                provider.reset_changes()
+
             if local_counter > 0:
                 changed = 1
+                if self.state == S_NEW:
+                    self.state = S_UPD
+
         return changed
-
-
